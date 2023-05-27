2e89b24a1c06d0ee223e595ee67b598efb68cbea
Anna <annahasagun@yahoo.com>
2023-05-27 20:21:34 +0300

    first commit

---
+++ HW1/HW1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <map>
+#include <unordered_map>
+#include <string>
+
+int main()
+{
+	std::string str;
+	std::cout << "[IN] " << '\n';
+	std::getline(std::cin, str);
+
+	std::unordered_map <char, int> ms;
+	for (const auto& c : str)
+	{
+		auto itr = ms.find(c);
+		if (itr == ms.end())
+		{
+			ms.insert({ c, 1 });
+		}
+		else
+		{
+			++((*itr).second);
+		}
+	}
+
+	std::multimap<int, char, std::greater<char>> mm;
+	for (const auto& [a, b] : ms)
+	{
+		mm.insert({ b, a });
+	}
+
+	std::cout << "[OUT] " << '\n';
+
+	for (const auto& [b, a] : mm)
+	{
+		std::cout << b << ": " << a << '\n';
+	}
+	return 0;
+}
---
+++ HW2/HW2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <set>
+
+int main()
+{
+	int size, n;
+	std::cout << "[IN] " << '\n';
+	std::cin >> size;
+	std::set<int,std::greater<int>> my_set;
+	for (int i = 0; i < size; ++i)
+	{
+		std::cin >> n;
+		my_set.insert(n);
+	}
+	std::cout << "[OUT] " << '\n';
+	for (const auto& elem : my_set)
+		std::cout << elem << '\n';
+	return 0;
+}
